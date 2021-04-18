5034564bce65cd793d91d0aab38142c3edcc4f8c
claffin <christian.laffin@gmail.com>
2021-04-19 00:32:02 +0100

    added better exception handling

--- cloudproxy/check.py
+++ cloudproxy/check.py
@@ -6,8 +6,11 @@ from cloudproxy.providers.digitalocean.config import username, password
 def check_alive(ip_address):
     auth = username + ":" + password
     proxies = {"http": "http://" + auth + "@" + ip_address + ":8899", "https": "http://" + auth + "@" + ip_address + ":8899"}
-    fetched_ip = requests.get("https://api.ipify.org", proxies=proxies).text
-    if ip_address == fetched_ip:
-        return True
-    else:
+    try:
+        fetched_ip = requests.get("https://api.ipify.org", proxies=proxies)
+        if ip_address == fetched_ip.text:
+            return True
+        else:
+            return False
+    except:
         return False

1912307d63285ad0c1a4c2b7bffbcffd0afc71d3
claffin <christian.laffin@gmail.com>
2021-04-18 23:16:15 +0100

    corrected bash script

--- cloudproxy/providers/digitalocean/user_data.sh
+++ cloudproxy/providers/digitalocean/user_data.sh
@@ -5,7 +5,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
 apt-get -y install docker-ce docker-ce-cli containerd.io
-docker run -d -p 8899:8899 --rm abhinavsingh/proxy.py:v2.3.1 --hostname 0.0.0.0 --basic-auth proxy:laffin
+docker run -d -p 8899:8899 --rm abhinavsingh/proxy.py:v2.3.1 --hostname 0.0.0.0 --basic-auth username:password
 ufw default deny incoming
 ufw default allow outgoing
 ufw allow 8899/tcp

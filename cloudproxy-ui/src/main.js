16dbc970d198448bbe3d635afc3ab91b9bb0c2c9
Christian <claffin@users.noreply.github.com>
2021-04-28 16:58:01 +0100

    Update cloudproxy-ui/src/main.js

--- cloudproxy-ui/src/main.js
+++ cloudproxy-ui/src/main.js
@@ -1,7 +1,7 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from "vue"
-import './plugins/bootstrap-vue'
+import "./plugins/bootstrap-vue"
 import App from './App.vue'
 import { ListGroupPlugin } from 'bootstrap-vue'
 Vue.use(ListGroupPlugin)

9ee6e8245461da3011959954ca72cf1321e9ff80
Mats Forsén <mats.forsen@datatrion.se>
2021-06-10 00:52:51 +0200

    Removed check_alive test until rewritten.

--- tests/test_check.py
+++
@@ -1,9 +0,0 @@
-from cloudproxy.check import check_alive
-
-
-def test_check_alive(mocker):
-    mocker.patch(
-        'cloudproxy.check.fetch_ip',
-        return_value="192.1.1.1"
-    )
-    assert check_alive("192.1.1.1") == True

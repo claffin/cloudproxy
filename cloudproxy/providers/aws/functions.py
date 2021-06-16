618f2cdc11fc644f8c86929361f450dc631f0fd3
Mats Forsén <mats.forsen@datatrion.se>
2021-06-16 20:52:46 +0200

    Change spot options to 'persistent' or 'one-time'

--- cloudproxy/providers/aws/functions.py
+++ cloudproxy/providers/aws/functions.py
@@ -43,7 +43,7 @@ def create_proxy():
         pass
     sg_id = ec2_client.describe_security_groups(GroupNames=["cloudproxy"])
     sg_id = sg_id["SecurityGroups"][0]["GroupId"]
-    if config["providers"]["aws"]["spot"] == 'True':
+    if config["providers"]["aws"]["spot"] == 'persistent':
         instance = ec2.create_instances(
             ImageId="ami-096cb92bb3580c759",
             MinCount=1,
@@ -55,13 +55,32 @@ def create_proxy():
             InstanceMarketOptions={
                 "MarketType": "spot",
                 "SpotOptions": {
-                    "InstanceInterruptionBehavior": "terminate",
-                    "SpotInstanceType": "one-time"
+                    "InstanceInterruptionBehavior": "stop",
+                    "SpotInstanceType": "persistent"
                 }
             },
             TagSpecifications=tag_specification,
             UserData=user_data,
         )
+    elif config["providers"]["aws"]["spot"] == 'one-time':
+            instance = ec2.create_instances(
+                ImageId="ami-096cb92bb3580c759",
+                MinCount=1,
+                MaxCount=1,
+                InstanceType=config["providers"]["aws"]["size"],
+                NetworkInterfaces=[
+                    {"DeviceIndex": 0, "AssociatePublicIpAddress": True, "Groups": [sg_id]}
+                ],
+                InstanceMarketOptions={
+                    "MarketType": "spot",
+                    "SpotOptions": {
+                        "InstanceInterruptionBehavior": "terminate",
+                        "SpotInstanceType": "one-time"
+                    }
+                },
+                TagSpecifications=tag_specification,
+                UserData=user_data,
+            )
     else:
         instance = ec2.create_instances(
             ImageId="ami-096cb92bb3580c759",
